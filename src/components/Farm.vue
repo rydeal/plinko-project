ecff0bdbade16017e293989e49dfdfd0e67273f0
Ryan Deal <rydeal@iu.edu>
2019-11-21 14:32:59 -0500

    move

---
+++ src/components/Farm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="farm">
+            <div v-for="plot in plots" v-bind:key="plot" class="plot">
+                {{ plot.plant }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import GameData from "../GameData.js"
+
+export default {
+    data: function() {
+        return {
+        plots: GameData.plots
+        }
+    },
+    name: "Farm"
+}
+</script>
+
+<style scoped>
+    .farm {
+        display: flex;
+    }
+
+    .plot {
+        width: 40px;
+        height: 40px;
+        margin: 2px;
+        border: 1px solid gainsboro;
+    }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            Home
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
