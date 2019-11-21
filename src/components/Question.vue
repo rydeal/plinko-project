0dc5574dd80dd0babb438744d2b86834f9e3ab75
hrlindse <lindsey.holly@yahoo.com>
2019-11-21 14:52:32 -0500

    Structured components

---
+++ src/components/Question.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+          <h1>Question</h1>
+          Value: {{value }}
+        </div>
+        <div>
+          <div class="question">
+            {{question}}
+            <form>
+              <input type="radio" v-for="answer in answers">
+              <button>Submit</button>
+            </form>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Question",
+    value: 1000
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div>
+            <h1>Result</h1>
+        </div>
+        <div class="result">
+          <h2>{{result}}</h2>
+          {{question}}
+          {{useranswer}}
+          {{correctanswer}}
+          {{pointsadded}} Points
+
+        </div>
+        <div class="scorecalc">
+          <h2>Score</h2>
+          Old Score:
+          +{{pointsadded}}
+          New Score:
+        </div>
+        <div class="button">
+          Next Turn
+          </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Result",
+    score: 10000,
+    result: "Correct",
+    question: "question",
+    useranswer: "User's Answer",
+    correctanswer: "Correct Answer",
+    pointsadded: "+1000"
+}
+</script>
+
+<style scoped>
+
+</style>
