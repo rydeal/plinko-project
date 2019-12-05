c8d74ad532c54620d253dab80f1beefad5951d34
Holly Lindsey <lindsey.holly@yahoo.com>
2019-12-05 13:27:52 -0500

    Update GameData.js

--- src/GameData.js
+++ src/GameData.js
@@ -1,9 +1,312 @@
 export default {
-    money: 4,
-    plots: [
-        { plant: null, stage: null },
-        { plant: null, stage: null },
-        { plant: null, stage: null },
-        { plant: null, stage: null }
+    "10": [
+      {
+        "question": "What was Play-Doh originally used as?",
+        "options": [
+          "Eraser",
+          "Modeling toy",
+          "Wallpaper cleaner",
+          "Leak plugger"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "When were Crocs invented?",
+        "options": [
+          "1993",
+          "2002",
+          "1999",
+          "1987"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "Who was the 30th President of the United States?",
+        "options": [
+          "William Howard Taft",
+          "Calvin Coolidge",
+          "Herbert Hoover",
+          "Grover Cleveland"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "Do you really want 10 points?",
+        "options": [
+          "Yes",
+          "No ",
+          "Maybe",
+          "I don't know"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "What percentage of the final grade is this project worth?\n",
+        "options": [
+          "15 ",
+          "10 ",
+          "20 ",
+          "25"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "How many keys are there on a traditional US keyboard?",
+        "options": [
+          "101 ",
+          "103",
+          "105",
+          "107"
+        ],
+        "answer": "0"
+      }
+    ],
+    "100": [
+      {
+        "question": "Which of the following is the correct file extension name for JavaScript?",
+        "options": [
+          ".script",
+          ".java",
+          ".js",
+          ".javascript"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "What is the language that most websites use for their layout?",
+        "options": [
+          "HTML ",
+          "XHTML",
+          "XML",
+          "JSON"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What programming language is used primarily for relational databases?",
+        "options": [
+          "SQL",
+          "CSS",
+          "C#",
+          "Python"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What JavaScript library have we used to render 3D objects on a web page?",
+        "options": [
+          "Babylon.js",
+          "Vue.js",
+          "Node.js",
+          "jQuery"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What year would a current college freshman with 0 credits graduate if they fell 2 semesters behind?",
+        "options": [
+          "2023",
+          "2024",
+          "2022",
+          "2025"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "Who is the current Chancellor of IUPUI?",
+        "options": [
+          "Mitch Daniels",
+          "Nasser Paydar",
+          "Michael McRobbie",
+          "James Danko"
+        ],
+        "answer": "1"
+      }
+    ],
+    "500": [
+      {
+        "question": "What Game Show does this game appear on?",
+        "options": [
+          "Wheel of Fortune",
+          "The Price is Right",
+          "Hollywood Game Night",
+          "Dotto"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "Who is the current host of this show?",
+        "options": [
+          "Drew Carey",
+          "Patrick Leonard Sajdak",
+          "Jack Narz",
+          "Jane Lynch"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What is the point value of this position squared?",
+        "options": [
+          "250,000",
+          "250.000",
+          "25,000",
+          "2,500,000"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What famous animal was shot and killed on May 28, 2016 after a child got into their enclosure?",
+        "options": [
+          "Okapi",
+          "Koko",
+          "Harambe",
+          "Clyde"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "What did Travis Faas write and publish a book on?",
+        "options": [
+          "Phaser.js",
+          "Pixi.js",
+          "melonJS",
+          "Kiwi.js"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "Who created the TV series SpongeBob SquarePants?",
+        "options": [
+          "Michael Dante DiMartino",
+          "Butch Hartman",
+          "Duncan Rouleau",
+          "Stephen Hillenburg"
+        ],
+        "answer": "3"
+      }
+    ],
+    "1000": [
+      {
+        "question": "Who created the World Wide Web?",
+        "options": [
+          "Martin Cooper",
+          "Tim Berners-Lee",
+          "Henry Sampson",
+          "John Logie Baird"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "When was the World Wide Web created?",
+        "options": [
+          "1971",
+          "1984",
+          "1975",
+          "1989"
+        ],
+        "answer": "3"
+      },
+      {
+        "question": "What organization creates the standards for the World Wide Web?",
+        "options": [
+          "Internet Architecture Board",
+          "ARINC",
+          "W3C",
+          "CERN"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "When did the creator of the World Wide Web die?",
+        "options": [
+          "April 21, 2016",
+          "June 25, 2009",
+          "February 11, 2012",
+          "They haven’t died yet"
+        ],
+        "answer": "3"
+      },
+      {
+        "question": "Who owns the World Bank?",
+        "options": [
+          "World Bank Group",
+          "International Finance Corporation",
+          "World Bank Programme",
+          "World Economic Forum"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "Where is St. Basil's Cathedral located?",
+        "options": [
+          "Moscow, Russia",
+          "London, England",
+          "New York City, United States",
+          "Reims, France"
+        ],
+        "answer": "0"
+      }
+    ],
+    "100000": [
+      {
+        "question": "What percentage of the Earth is covered by water?",
+        "options": [
+          "83",
+          "71 ",
+          "64",
+          "59"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "What is the formula for potassium chromate?",
+        "options": [
+          "K2CrO4 ",
+          "K2Cr2O7",
+          "C2H3O4",
+          "C2H3O2"
+        ],
+        "answer": "0"
+      },
+      {
+        "question": "What is 1 MeV in eV?",
+        "options": [
+          "2.5721x10^6 eV",
+          "1x10^6 eV",
+          "1x10^9 eV",
+          "2.5721x10^9 eV"
+        ],
+        "answer": "1"
+      },
+      {
+        "question": "Which of the following statements is NOT true regarding the comparison of the alpha-helix structure to the beta-sheet structure in proteins?",
+        "options": [
+          "The peptide bond in each is planar and trans",
+          "Each may occur in typical globular proteins",
+          "Each is stabilized by inter-chain hydrogen bonds",
+          "All hydrogen bonds between the carbonyl oxygen (C=O) and the amide hydrogen (N-H) are formed in each"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "What does the word provocados mean in English?",
+        "options": [
+          "sea crossing",
+          "brought about",
+          "self-induced abortions",
+          "premature newborn"
+        ],
+        "answer": "2"
+      },
+      {
+        "question": "Who coined the term Oedipus Complex?",
+        "options": [
+          "Sigmund Freud",
+          "Lev Vygotsky",
+          "Lawrence Kohlberg",
+          "Philip Zimbardo"
+        ],
+        "answer": "0"
+      }
     ]
-}
+  }
