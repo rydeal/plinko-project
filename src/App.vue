aa8d07832d8f6c5c038c926db30ad9a3e9aa919f
Holly Lindsey <lindsey.holly@yahoo.com>
2019-12-12 15:05:21 -0500

    Positioning, routing

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,150 @@
 <template>
   <div id="app">
-  <form action="/question" >
-    <input id="10" type="radio" name="score" value="10"><label for="10">10 points</label>
-    <input id="100" type="radio" name="score" value="100"><label for="100">100 points</label>
-    <input type="submit" value="Submit"> 
-  </form>
-  <router-link to="/question/10">Question 10</router-link>
-    <router-view></router-view>
+    <div class="board">
+      <img :src="image" />
+    </div>
+    <form>
+      <div class="options">
+        <input id="10" type="radio" name="score" value="10" v-model="value" />
+        <label for="10">
+          10 points
+        </label>
+        <br />
+        <input id="100" type="radio" name="score" value="100" v-model="value" />
+        <label for="100">
+          100 points
+        </label>
+        <br />
+        <input id="500" type="radio" name="score" value="500" v-model="value" />
+        <label for="500">
+          500 points
+        </label>
+        <br />
+        <input
+          id="1000"
+          type="radio"
+          name="score"
+          value="1000"
+          v-model="value"
+        />
+        <label for="1000">
+          1,000 points
+        </label>
+        <br />
+        <input
+          id="100000"
+          type="radio"
+          name="score"
+          value="100000"
+          v-model="value"
+        />
+        <label for="100000">
+          100,000 points
+        </label>
+      </div>
+      <br />
+      <br />
+      <button @click="routerOpen = !routerOpen">
+        <router-link v-bind:to="'/question/' + value"
+          >Question Value:
+          {{
+            value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          }}</router-link
+        >
+      </button>
+    </form>
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <div v-show="routerOpen" class="overlay">
+      <font-awesome-icon
+        v-on:click="routerOpen = !routerOpen"
+        class="close"
+        icon="times"
+      />
+      <router-view
+        v-show="routerOpen"
+        v-closable="{
+          exclude: ['button'],
+          handler: 'onClose'
+        }"
+        class="popup-container"
+      ></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import image from "./assets/plinko-board.jpg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+library.add(faTimes);
 
 export default {
-  name: 'app'
-}
+  name: "app",
+  data: function() {
+    return { value: 10, image: image, routerOpen: false };
+  },
+  methods: {
+    getFormValues(submitEvent) {
+      this.value = submitEvent.target.elements.name.value;
+    },
+    onClose() {
+      this.routerOpen = false;
+    },
+    routerToggle() {
+      this.routerOpen = !this.routerOpen;
+    }
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+
+.options {
+  display: inline-block;
+  text-align: left;
+}
+
+.popup-container {
+  position: fixed;
+  width: 60vw;
+  height: 70vh;
+  background-color: white;
+  top: 0;
+  margin-top: 15vh;
+  left: 19.2vw;
+  border-radius: 25px;
+  border: 2px solid black;
+}
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.hidden {
+  visibility: hidden;
+}
+.close {
+  font-size: 50px;
+  color: black;
+  position: fixed;
+  top: 16.5vh;
+  z-index: 99;
+  left: 73.5vw;
 }
 </style>
--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,55 +1,56 @@
 <template>
+  <div>
     <div>
-        <div>
-          <h1>Question</h1>
-          Question Num: {{ questionNum}}
-          Value: {{ value }}
-        </div>
-        <div>
-          <div class="question">
-            {{question}}
-            <form>
-            <div class="options">
-            <div class="option" v-for="(option, index) in options" v-bind:key="option" >
-              <input :id="index" name="option" type="radio">
-              <label :for="index">{{option}}</label>
-            </div>
-            </div>
-              <button>Submit</button>
-            </form>
-          </div>
-        </div>
+      <h1>Question</h1>
+      Question Num: {{ questionNum }} Value: {{ value }}
     </div>
+    <div>
+      <div class="question">
+        {{ question }}
+        <form>
+          <div class="options">
+            <div
+              class="option"
+              v-for="(option, index) in options"
+              v-bind:key="option"
+            >
+              <input :id="index" name="option" type="radio" />
+              <label :for="index">{{ option }}</label>
+            </div>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
 import GameData from "../GameData.js";
 var questionNum;
 
 export default {
-  data: function () {
+  data: function() {
     return {
-    questionNum: questionNum,
-    question: GameData[10][questionNum].question,
-    value: 10,
-    options: ["option"]
-    }
-  }, 
+      questionNum: questionNum,
+      question: GameData[10][questionNum].question,
+      value: 10,
+      options: ["option"]
+    };
+  },
   created: function() {
-  questionNum=0;
+    questionNum = 0;
     this.randQ();
   },
-  methods:{
+  methods: {
     randQ() {
-      questionNum=  (Math.random()*6)-1;
+      questionNum = Math.random() * 6 - 1;
+    },
+    getFormValues(submitEvent) {
+      this.value = submitEvent.target.elements.name.value;
     }
   }
-  
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
