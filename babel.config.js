4ac871b73270dd1d3c9507673e6b7f715b791194
Holly Lindsey <lindsey.holly@yahoo.com>
2019-12-14 01:59:51 -0500

    kk

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  publicPath: process.env.NODE_ENV === "production" ? "/plinko-project/" : "/"
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
