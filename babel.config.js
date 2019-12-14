5c0b1bfb0154eeab506610058ff7f81ab3d78369
Ryan Deal <rydeal@iu.edu>
2019-12-14 01:04:08 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,6 @@
-module.exports = { presets: ["@vue/cli-plugin-babel/preset"] };
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/plinko-project/'
+  : '/'
+};
