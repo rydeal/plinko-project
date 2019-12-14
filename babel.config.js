8ffaa485afa6a4fa89f6782ebcdaa6be2e5951a2
Holly Lindsey <lindsey.holly@yahoo.com>
2019-12-14 01:49:07 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
+  baseUrl: "/my-first-project/",
   presets: ["@vue/cli-plugin-babel/preset"],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/plinko-project/'
-  : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/plinko-project/" : "/"
 };
